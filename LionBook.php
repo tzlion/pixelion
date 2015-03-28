7a0ebd5269f827a37040f961b143ce077a8bf778
taizou <cactusgunman@gmail.com>
2015-03-28 13:28:57 +0000

    Rewritten LionBook.php

--- LionBook.php
+++ LionBook.php
@@ -1,59 +1,26 @@
 <?php
-/**
- * LionBook nouveau.
- *
- * Translated from gwicke's previous TAL template version to remove
- * dependency on PHPTAL.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
- * @file
- * @ingroup Skins
- */
 
-$wgExtensionCredits['skin'][] = array(
-	'path' => __FILE__,
-	'name' => 'LionBook',
-	'namemsg' => 'skinname-lionbook',
-	'descriptionmsg' => 'lionbook-desc',
-	'url' => 'http://lion.li',
-	'author' => array( 'a lion', '...' ),
-	'license-name' => 'GPLv2+',
+$wgExtensionCredits['skin'][] = array (
+    'path' 		=> __FILE__,
+    'name' 		=> 'LionBook',
+    'namemsg' 	=> 'skinname-lionbook',
+    'descriptionmsg' => 'lionbook-desc',
+    'url' 		=> 'http://lion.li',
+    'author' 	=> array( 'a lion' ),
+    // 'license_name' => 'idk yet'
 );
 
-// Register files
-$wgAutoloadClasses['SkinLionBook'] = __DIR__ . '/SkinLionBook.php';
-$wgAutoloadClasses['LionBookTemplate'] = __DIR__ . '/LionBookTemplate.php';
-$wgMessagesDirs['LionBook'] = __DIR__ . '/i18n';
+$wgAutoloadClasses['SkinLionBook'] 		= __DIR__ . '/SkinLionBook.php';
+$wgAutoloadClasses['LionBookTemplate'] 	= __DIR__ . '/LionBookTemplate.php';
+$wgMessagesDirs['LionBook'] 			= __DIR__ . 'i18n';
 
-// Register skin
 $wgValidSkinNames['lionbook'] = 'LionBook';
 
-// Register modules
-$wgResourceModules['skins.lionbook.styles'] = array(
-	'styles' => array(
-		'main.css' => array( 'media' => 'screen' ),
-	),
-	'remoteSkinPath' => 'LionBook',
-	'localBasePath' => __DIR__,
-);
 $wgResourceModules['skins.lionbook.harrystyles'] = array(
-	'styles' => array(
-		'lion.css' => array( 'media' => 'screen' ),
-	),
-	'remoteSkinPath' => 'LionBook',
-	'localBasePath' => __DIR__,
+    'styles' => array(
+        'lion.css' => array( 'media' => 'screen' ),
+        // 'printlion.css' => array( 'media' => 'print' ), // maybe later
+    ),
+    'remoteSkinPath' 	=> 'LionBook',
+    'localBasePath' 	=> __DIR__,
 );
